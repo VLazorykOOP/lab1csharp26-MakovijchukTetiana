589065dbff394c2768ad6047bd2c7e8cc80e1004
MakovijchukTetiana <makoviichuk.tetiana@chnu.edu.ua>
2026-03-11 21:33:14 +0200

    Refactor Program.cs to include task selection menu

--- Lab1Charp/Program.cs
+++ Lab1Charp/Program.cs
@@ -1,16 +1,73 @@
-﻿// See https://aka.ms/new-console-template for more information
+using System;
 
- static void task1()
+namespace lab1
 {
-    Console.WriteLine("Task1 !");
-    Console.Write("s= ");
-    string? str = Console.ReadLine();
-    float s = 0; 
-    if (str != null) s = float.Parse(str);
-    double p = 4 * Math.Sqrt(s);
-    Console.WriteLine("p=" + p);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Налаштування кодування 
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
 
+            bool keepRunning = true;
+
+            while (keepRunning)
+            {
+                // Виводимо красиве меню
+               
+                Console.WriteLine("         Лабораторна робота №1");
+                Console.WriteLine("Оберіть завдання для перевірки:");
+                Console.WriteLine("1. Завдання 1.10 (Ребро куба)");
+                Console.WriteLine("2. Завдання 2.10 (Сума цифр двозначного числа)");
+                Console.WriteLine("3. Завдання 3.10 (Точка в області)");
+                Console.WriteLine("4. Завдання 4.10 (Пошук невідомої дати)");
+                Console.WriteLine("5. Завдання 5.10 (Квадрат суми двох чисел)");
+                Console.WriteLine("6. Завдання 6.10 (Обчислення виразу)");
+                Console.WriteLine("0. Вихід з програми");
+               
+                Console.Write("Ваш вибір: ");
+
+                string choice = Console.ReadLine();
+                Console.WriteLine(); 
+
+                // Викликаємо метод Execute() відповідного класу залежно від вибору
+                switch (choice)
+                {
+                    case "1":
+                        Task1.Execute();
+                        break;
+                    case "2":
+                        Task2.Execute();
+                        break;
+                    case "3":
+                        Task3.Execute();
+                        break;
+                    case "4":
+                        Task4.Execute();
+                        break;
+                    case "5":
+                        Task5.Execute();
+                        break;
+                    case "6":
+                        Task6.Execute();
+                        break;
+                    case "0":
+                        keepRunning = false;
+                        Console.WriteLine("Дякую за використання програми. До побачення!");
+                        break;
+                    default:
+                        Console.WriteLine("Невідомий вибір. Будь ласка, введіть число від 0 до 6.");
+                        break;
+                }
+
+                // Затримка екрану перед поверненням до головного меню
+                if (keepRunning)
+                {
+                    Console.WriteLine("\nНатисніть будь-яку клавішу для повернення в меню...");
+                    Console.ReadKey();
+                    Console.Clear(); // Очищуємо консоль
+                }
+            }
+        }
+    }
 }
-Console.WriteLine("Lab 1 !");
-task1();
-// continue ...
